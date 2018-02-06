7dabffe97d35fa373a70da333104fc410953edf6
grant lodge <grant.lodge@outlook.com>
2018-02-06 21:06:12 +0000

    trying to do better code :S

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -4,11 +4,11 @@ import javax.inject._
 
 import akka.actor.ActorSystem
 import models.User
+import play.api.libs.json.Json
 import play.api.mvc._
 import services.Users
-import play.api.libs.json._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import scala.util.Try
 
 case class UserModel(username: String, id: Int)
@@ -18,15 +18,27 @@ case class UserPassModel(username: String, password: String)
 @Singleton
 class UserController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem, users: Users)(implicit exec: ExecutionContext) extends AbstractController(cc) {
 
-  def getUser: Action[AnyContent] = Action.async((req) => {
-    val username = req.queryString("username").head
+  //  def funx(y: Try[Option[User]]): Result = {
+  //    y.fold(fa => InternalServerError, fb => fb.fold(NotFound).)
+  //  }
 
-    val x: Future[Try[Option[User]]] = users.getUser(username)
-    x.map(z => Ok(Json.toJson(z.get.get.toPublic)))
+  def getUser: Action[AnyContent] = Action.async((req) => {
+    //    val x: Future[Try[Option[User]]] =
+    //      users.getUser(username).map{x => funx(x) }
+    //    users.getUser(username).map(s => s.fold[Status]())
+    val username = req.queryString("username").head
+    users.getUser(username)
+      .map(f => f.fold(
+        _ => InternalServerError.apply(""),
+        fb => fb.fold(
+          NotFound.apply(""))(
+          fb => Ok.apply(Json.stringify(fb.toPublic))))
+      )
   })
 
+
   def addUser: Action[AnyContent] = Action.async((req) => {
-    val user = User("","", 0, true)
+    val user = User("", "", 0, true)
     users.addUser(user.username, user.password).map(_ => Created)
   })
 

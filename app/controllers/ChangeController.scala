c5b76581e53dddf5afa4b07f19d676f0e07b3cc0
Grant.Lodge <grant.lodge@dotmailer.com>
2018-02-26 17:13:40 +0000

    hmm

--- app/controllers/ChangeController.scala
+++ app/controllers/ChangeController.scala
@@ -14,11 +14,24 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class ChangeController @Inject() (cc: ControllerComponents, actorSystem: ActorSystem, changes: Changes)(implicit exec:ExecutionContext) extends AbstractController(cc) {
 
+  def skipValidation(skipVal: Request[AnyContent]): Option[String] = None
+  def takeValidation(skipVal: Request[AnyContent]): Option[String] = None
+
   def getExample(): Action[AnyContent] = Action.async { x =>
     val jsValue: JsValue = Json.parse("{\"val\" : \"x\"}")
     val change = Change(
       1, 1, 1, 1, jsValue, DateTime.now)
 
     Future.successful("").map(_ => Ok(Json.stringify(change)))
+  }
+
+  def get(): Action[AnyContent] = Action.async { x =>
+
+    val skipValidationFailure = skipValidation(x)
+
+    if (skipValidationFailure.nonEmpty) Future.successful("").map(_ => BadRequest(skipValidationFailure.get))
+    else{
+      changes.getAllChanges("", 0, 1000).map(_ => BadRequest(""))
+    }
   }
 }

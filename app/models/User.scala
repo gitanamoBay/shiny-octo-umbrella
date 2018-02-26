262fccd34574986b93a74e4088d25f08b618829b
Grant.Lodge <grant.lodge@dotmailer.com>
2018-02-26 17:00:55 +0000

    changes

--- app/models/User.scala
+++ app/models/User.scala
@@ -8,17 +8,13 @@ case class User(username: String, password: String, id: Int, enabled: Boolean) {
   def toPublic: PublicUser = PublicUser(username, id)
 }
 
-case class PublicUser(username: String, id: Int)
-
-object PublicUser {
+case class PublicUser(username: String, id: Int) {
   implicit val format: OFormat[PublicUser] = Json.format[PublicUser]
 
   implicit def publicUserToJson(u: PublicUser): JsValue = Json.toJson(u)
 }
 
 
-case class DefineUser(username: String, password: String)
-
-object DefineUser {
+case class DefineUser(username: String, password: String) {
   implicit val format: OFormat[DefineUser] = Json.format[DefineUser]
 }
--- app/services/ChangeService.scala
+++ app/services/ChangeService.scala
@@ -3,16 +3,35 @@ package services
 import javax.inject._
 
 import models.Change
+import org.apache.commons.lang3.NotImplementedException
 import scalikejdbc._
 
 import scala.concurrent.Future
 import scala.util.Try
 
 trait Changes {
+  def getAllChanges(filter: => String, skip: Int, take: Int): Future[Try[Option[Array[Change]]]]
+
+  def addChange(change: Change): Future[Try[Option[Boolean]]]
+
+  def updateChange(change: Change): Future[Try[Option[Boolean]]]
 
 }
 
 @Singleton
-class ChangeService @Inject()() extends Changes{
+class ChangeService @Inject()() extends Changes {
 
+  def getAllChanges(filter: => String, skip: Int, take: Int): Future[Try[Option[Array[Change]]]] = {
+    Future.successful(Try {
+      throw NotImplementedException
+    })
+  }
+
+  override def addChange(change: Change): Future[Try[Option[Boolean]]] = Future.successful(Try {
+    throw NotImplementedException
+  })
+
+  override def updateChange(change: Change): Future[Try[Option[Boolean]]] = Future.successful(Try {
+    throw NotImplementedException
+  })
 }
